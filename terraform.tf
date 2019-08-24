15d79babceb72024966f6d86f31a6257032ea80e
Mitchell Paterson <mitchell.paterson@rea-group.com>
2019-08-24 15:40:27 +1000

    Add terraform

--- terraform.tf
+++ terraform.tf
@@ -9,6 +9,37 @@ resource "aws_instance" "connect4" {
   key_name = "connect4"
   vpc_security_group_ids = ["sg-013088ddfb67a3198"]
   subnet_id = "subnet-b5b7a5d2"
+  iam_instance_profile = "Connect4"
+
+  provisioner "file" {
+
+    connection {
+      host = aws_instance.connect4.public_ip
+      type = "ssh"
+      user = "ec2-user"
+      private_key = file("connect4.pem")
+      timeout = "10m"
+      agent = false
+    }
+
+    source      = "encrypted/prod.encrypted"
+    destination = "prod.encrypted"
+  }
+
+  provisioner "file" {
+
+    connection {
+      host = aws_instance.connect4.public_ip
+      type = "ssh"
+      user = "ec2-user"
+      private_key = file("connect4.pem")
+      timeout = "10m"
+      agent = false
+    }
+
+    source      = "auto/remote"
+    destination = "remote"
+  }
 
   provisioner "remote-exec" {
 
@@ -22,14 +53,9 @@ resource "aws_instance" "connect4" {
     }
 
     inline = [
-      "sudo yum update -y",
-      "sudo yum install docker -y",
-      "sudo usermod -a -G docker ec2-user",
-      "sudo setfacl -m user:ec2-user:rw unix:///var/run/docker.sock:",
-      "sudo service docker start",
-      "sudo docker pull mtchd/connect4",
-
-      "docker run --rm -it -e ${TOKEN} connect4"
+      "chmod +x remote",
+      "./remote",
     ]
+
   }
 }
